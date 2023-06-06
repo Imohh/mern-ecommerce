3a9c87410912e3e452d226e9e8a8e3747c76c227
Iyimide <adekileiyimide@gmail.com>
2023-06-07 00:37:14 +0100

    categories section done

--- client/app/containers/Homepage/index.js
+++ client/app/containers/Homepage/index.js
@@ -13,7 +13,9 @@ import actions from '../../actions';
 import banners from './banners.json';
 import CarouselSlider from '../../components/Common/CarouselSlider';
 import { responsiveOneItemCarousel } from '../../components/Common/CarouselSlider/utils';
-import background from './ron-lach.jpg'
+import background from './assets/ron-lach.jpg'
+import bottomImg from './assets/bottoms.header.jpg'
+import accessoriesImg from './assets/accessorizz.jpg'
 import { ArrowBackIcon } from '../../components/Common/Icon';
 
 class Homepage extends React.PureComponent {
@@ -73,14 +75,14 @@ class Homepage extends React.PureComponent {
         </div>
 
         <div className='categories'>
-          <Row className='flex-col'>
+          <Row className='flex-row' >
             <Col xs='12' lg='12'>
-                <h2>Shop by category</h2>  
-                <p>T-shirts, bags, accessories , we've got it all</p> 
+                <h2>Kentyle categories</h2>  
+                <p>T-shirts, pants, accessories , we've got it all</p> 
                 <button>Shop now</button>     
             </Col>
             <Col lg = '12' className='categories-image categories-image1' style={{ backgroundImage: `url(${background})` }}>
-                <p>UPPERS</p>
+                <p>TOP</p>
                 <div className='content'>
                   <div class="content-overlay"></div>
                   <div class="content-details fadeIn-bottom">
@@ -91,11 +93,32 @@ class Homepage extends React.PureComponent {
                   </div>
                 </div>
             </Col> 
-            <Col lg= '6' className='categories-image categories-image2'
-            style={{}}>
+            <Col lg= '4' className='categories-image categories-image2'
+            style={{backgroundImage: `url(${bottomImg})`}}>
+              <p>BOTTOM</p>
+                <div className='content'>
+                  <div class="content-overlay"></div>
+                  <div class="content-details fadeIn-bottom">
+                    <h3>Jeans</h3>
+                    <h3>Chinose</h3>
+                    <h3>Cordurouy</h3>
+                    <ArrowBackIcon className='icon-go' width='50' height='30'/>
+                  </div>
+                </div>
             </Col>
-            <Col lg = '6' className='categories-image categories-image2'
-            style={{}}>
+            <Col lg = '7' className='categories-image categories-image3'
+            style={{backgroundImage: `url(${accessoriesImg})`}}>
+                <p>Accessories</p>
+                <div className='content'>
+                  <div class="content-overlay"></div>
+                  <div class="content-details fadeIn-bottom">
+                    <h3>Sun glasses</h3>
+                    <h3>Balaclavas</h3>
+                    <h3>Caps & Hats</h3>
+                    <h3>Rings</h3>
+                    <ArrowBackIcon className='icon-go' width='50' height='30'/>
+                  </div>
+                </div>
             </Col>
           </Row>
         </div>
