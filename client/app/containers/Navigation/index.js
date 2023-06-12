4945ff1726bdfdbf2b302d6a8ae7454d132efcb7
Imoh Precious <jaypee88830@gmail.com>
2023-06-12 17:36:21 +0100

    admin panel fix

--- client/app/containers/Navigation/index.js
+++ client/app/containers/Navigation/index.js
@@ -172,7 +172,7 @@ class Navigation extends React.PureComponent {
                   {authenticated ? (
                     <UncontrolledDropdown nav inNavbar>
                       <DropdownToggle nav>
-                        {user.firstName ? user.firstName : 'Welcome'}
+                        <img className="account-image" width="24" height="24" src="https://img.icons8.com/material-outlined/24/user--v1.png" alt="user--v1"/><span className="hide-display">{user.firstName ? user.firstName : 'Welcome'}</span>
                         <span className='fa fa-chevron-down dropdown-caret'></span>
                       </DropdownToggle>
                       <DropdownMenu right>
