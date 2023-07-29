d9c2b0e1933fee0293df0a0a9a18793ac4cba43c
Imoh Precious <jaypee88830@gmail.com>
2023-07-29 04:15:33 +0100

    production link

--- client/app/constants/index.js
+++ client/app/constants/index.js
@@ -2,7 +2,7 @@ export const BASE_API_URL = process.env.BASE_API_URL;
 
 export const SOCKET_URL =
   window.location.host.indexOf('localhost') >= 0
-    ? 'http://127.0.0.1:3000'
+    ? 'https://mern-ecommerce-fawn.vercel.app/'
     : window.location.host;
 
 export const ROLES = {
