6abe3aa08616e6ddd29dc2857776201dd3eb31a2
Imoh Precious <jaypee88830@gmail.com>
2023-08-30 05:57:19 +0100

    netlify deploy

--- client/app/constants/index.js
+++ client/app/constants/index.js
@@ -2,7 +2,7 @@ export const BASE_API_URL = 'api';
 
 export const SOCKET_URL =
   window.location.host.indexOf('localhost') >= 0
-    ? 'https://mern-ecommerce-05ka.vercel.app'
+    ? 'https://main--cute-sfogliatella-eefa6f.netlify.app/'
     : window.location.host;
 
 export const ROLES = {
