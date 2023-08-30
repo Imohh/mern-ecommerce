d7b2c4c30cbc03a6a4a70328301922e188361554
Imoh Precious <jaypee88830@gmail.com>
2023-08-30 16:35:07 +0100

    routes

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,7 +2,7 @@ module.exports = {
   app: {
     name: 'Mern Ecommerce',
     apiURL: 'api',
-    serverURL: 'https://ecommerce-server-lemon.vercel.app',
+    serverURL: 'https://mern-ecommerce-o5ka.vercel.app',
     clientURL: 'http://localhost:8080'
   },
   port: 3000,
